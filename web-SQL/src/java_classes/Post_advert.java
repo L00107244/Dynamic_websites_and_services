61a0f5248333c92978ab7304ecc456da32f5ae94
L00107244 <L00107244@student.lyit.ie>
2017-08-07 21:32:03 +0100

    added some more sql methods next is to finish sql and fix bugs

--- web-SQL/src/java_classes/Post_advert.java
+++ web-SQL/src/java_classes/Post_advert.java
@@ -4,6 +4,7 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 import com.mysql.jdbc.Blob;
@@ -20,6 +21,7 @@ public class Post_advert
   private String phone_number;
   private String images;
   private int seller_id;
+  private String categories;
   
 	 // JDBC driver name and database URL
 	 static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
@@ -32,7 +34,7 @@ public class Post_advert
 	 static final String USER = "root";
 	 static final String PASS = "password";
   
-  public Post_advert(String namein, String condin, String Descrip, String locat, String em, String pn, int sid)
+  public Post_advert(String namein, String condin, String Descrip, String locat, String em, String pn, int sid, String cat)
   {
 	this.Name = namein;
 	this.condition = condin;
@@ -41,8 +43,9 @@ public class Post_advert
 	this.E_mail = em;
 	this.phone_number = pn;
 	this.seller_id = sid;
+	this.categories = cat;
   }
-  public Post_advert( int id,String namein, String condin, String Descrip, String locat, String em, String pn, int sid)
+  public Post_advert( int id,String namein, String condin, String Descrip, String locat, String em, String pn, int sid, String cate)
   {
 	this.item_id = id;
 	this.Name = namein;
@@ -52,6 +55,7 @@ public class Post_advert
 	this.E_mail = em;
 	this.phone_number = pn;
 	this.seller_id = sid;
+	this.categories = cate;
   }
   public Post_advert()
   {
@@ -89,6 +93,10 @@ public class Post_advert
   {
 	  seller_id = idin;
   }
+  public void setCategories(String catin)
+  {
+	  categories = catin;
+  }
   public int getID()
   {
 	  return item_id;
@@ -125,16 +133,21 @@ public class Post_advert
   {
 	  return seller_id;
   }
+  public String getCate()
+  {
+	  return categories;
+  }
 	public static int add(Post_advert pad)
 	{
 
 			int status = 0;
-			String sqlString= "insert into sell( Name, condition,Description, Location, E_mail, phone_number, sellere_id) values(\'"+pad.getName()+ 
+			String sqlString= "insert into sell( Name, condition,Description, Location, E_mail, phone_number, sellere_id, categories) values(\'"+pad.getName()+ 
 					"\', \'"+pad.getCondition()+ 
 					"\', \'"+pad.getDescription() + 
 					"\', \'"+pad.getLocation() +
 					"\', \'"+pad.getEmail() +
 					"\', \'"+pad.getPhoneNumber() +
+					"\', \'"+pad.getCate() +
 					"\', \'"+ pad.getSellerid()+"\')";
 			status = databaseUpdate(sqlString);
 			System.out.print(sqlString);  
@@ -158,7 +171,8 @@ public class Post_advert
              		    	    resultSet.getString("Location"),
              		    	    resultSet.getString("E_mail"),
              		    	    resultSet.getString("phone_number"),
-             		    	    resultSet.getInt("sellere_id")); 
+             		    	    resultSet.getInt("sellere_id"),
+             		    	    resultSet.getString("categories")); 
              		    	                  
            }   
            conn.close();
@@ -167,6 +181,39 @@ public class Post_advert
           e.printStackTrace();
        }  
        return pa;
+		
+	}
+	public static ArrayList<Post_advert>SearchByWords(String name, String tech, String Locat)
+	{
+	  Post_advert pa = new Post_advert();
+	  ArrayList<Post_advert> arr = new ArrayList<Post_advert>();
+	  try
+	  {
+          Class.forName("com.mysql.jdbc.Driver");
+          Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+          statement=conn.createStatement();
+
+          resultSet=statement.executeQuery("SELECT * FROM `sell` WHERE Name ='"+name+"'&&categories='"+tech+"'&&Location='"+Locat+"'");
+          while ( resultSet.next() )      {
+              pa = new Post_advert(
+            		            resultSet.getString("Name"),   
+             		    	    resultSet.getString("condition"),
+             		    	    resultSet.getString("Description"),
+             		    	    resultSet.getString("Location"),
+             		    	    resultSet.getString("E_mail"),
+             		    	    resultSet.getString("phone_number"),
+             		    	    resultSet.getInt("sellere_id"),
+             		    	    resultSet.getString("categories")); 
+                                
+              arr.add(pa);
+             		    	                  
+           }   
+           conn.close();
+	  }
+      catch (Exception e) {
+          e.printStackTrace();
+       }  
+       return arr;
 		
 	}
     public static int delete(int itemidIn) 
@@ -201,27 +248,11 @@ public class Post_advert
 		}
 		return status;
 	}
-	  public static void main(String []args)
-	  {
-		  Post_advert search = new Post_advert();
-		  int i = 13;
-		  search.setSellerid(i);
-		  int idin = 13;
-			Post_advert SearchNotes = Post_advert.getAdvertsByID(idin);
-    
-			System.out.println(SearchNotes.getID() + " \t" +
-					SearchNotes.getID() + " \t" + 
-					SearchNotes.getSellerid() + " \t" +
-					SearchNotes.getCondition() + " \t" +
-					SearchNotes.getDescription() + " \t" +
-					SearchNotes.getLocation() + " \t" +
-					SearchNotes.getEmail() + " \t" +
-					SearchNotes.getPhoneNumber() + " \t" +
-					SearchNotes.getName() );
-			   int del = 2;
-			   search.delete(del);
-		}
-	
-	   
-	  }
+	@Override
+	public String toString()
+	{
+		return " "+item_id+" "+Name+" "+condition+" "+Description+" "+Location+" "+E_mail+" "+phone_number+" "+" "+categories+" "+seller_id;
+	}
+ 
+}
 
